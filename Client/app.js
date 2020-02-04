fc6e911ba66e83dfde66aaa13393a6be5d955f0b
ardit bijwaard <ardittristan@gmail.com>
2020-02-04 16:48:37 +0100

    fix deletions

--- Client/app.js
+++ Client/app.js
@@ -9,19 +9,19 @@ var serverIp = "http://127.0.0.1:3333";
 var img = [
     34862,
     30281
-]
+];
 
 // Initialize the map
 
-var map = L.map('map')
+var map = L.map('map');
 
-var rc = new L.RasterCoords(map, img)
-map.setMaxZoom(rc.zoomLevel())
-map.setView(rc.unproject([img[0], img[1]]), 3)
+var rc = new L.RasterCoords(map, img);
+map.setMaxZoom(rc.zoomLevel());
+map.setView(rc.unproject([img[0], img[1]]), 3);
 
 L.tileLayer('./tiles/{z}/{x}/{y}.png', {
     attribution: '',
-    bounds: [[0,-180], [86, 12]],
+    bounds: [[0, -180], [86, 12]],
     noWrap: true
 }).addTo(map);
 
@@ -41,95 +41,99 @@ var drawControl = new L.Control.Draw({
 map.addControl(drawControl);
 
 //! create old objects on page load
-setTimeout(fetchOldMarkers, 1000);
+setTimeout(fetchOldMarkers, 100);
 async function fetchOldMarkers() {
     //* fetch polylines
     fetch(`${serverIp}/fetchpolyline`)
-    .then(async function(response) {
-        var responseJSON = await response.json()
-        var array = responseJSON.polyline
-        if (array.length != 0) {
-            array.forEach(element => {
-                elementArray = JSON.parse(element)
-                L.polyline(elementArray.geometry.coordinates, {
-                    allowIntersection: true,
-                    repeatMode: false,
-                    drawError: {
-                        color: '#b00b00',
-                        timeout: 2500
-                    },
-                    icon: new L.DivIcon({
-                        iconSize: new L.Point(8, 8),
-                        className: 'leaflet-div-icon leaflet-editing-icon'
-                    }),
-                    touchIcon: new L.DivIcon({
-                        iconSize: new L.Point(20, 20),
-                        className: 'leaflet-div-icon leaflet-editing-icon leaflet-touch-icon'
-                    }),
-                    guidelineDistance: 20,
-                    maxGuideLineLength: 4000,
-                    shapeOptions: {
-                        stroke: true,
-                        color: '#3388ff',
-                        weight: 4,
-                        opacity: 0.5,
-                        fill: false,
-                        clickable: true
-                    },
-                    metric: true,
-                    feet: true,
-                    nautic: false,
-                    showLength: true,
-                    zIndexOffset: 2000,
-                    factor: 1,
-                    maxPoints: 0
-                }).addTo(drawnItems);
-            });
-        }
-    });
+        .then(async function (response) {
+            var responseJSON = await response.json();
+            var array = responseJSON.polyline;
+            if (array.length != 0) {
+                array.forEach(async element => {
+                    var coordinates = [];
+                    elementArray = await JSON.parse(element);
+                    elementArray.forEach(inputCoordinate => {
+                        coordinates.push(rc.unproject(inputCoordinate))
+                    })
+                    L.polyline(coordinates, {
+                        allowIntersection: true,
+                        repeatMode: false,
+                        drawError: {
+                            color: '#b00b00',
+                            timeout: 2500
+                        },
+                        icon: new L.DivIcon({
+                            iconSize: new L.Point(8, 8),
+                            className: 'leaflet-div-icon leaflet-editing-icon'
+                        }),
+                        touchIcon: new L.DivIcon({
+                            iconSize: new L.Point(20, 20),
+                            className: 'leaflet-div-icon leaflet-editing-icon leaflet-touch-icon'
+                        }),
+                        guidelineDistance: 20,
+                        maxGuideLineLength: 4000,
+                        shapeOptions: {
+                            stroke: true,
+                            color: '#3388ff',
+                            weight: 4,
+                            opacity: 0.5,
+                            fill: false,
+                            clickable: true
+                        },
+                        metric: true,
+                        feet: true,
+                        nautic: false,
+                        showLength: true,
+                        zIndexOffset: 2000,
+                        factor: 1,
+                        maxPoints: 0
+                    }).addTo(drawnItems);
+                });
+            }
+        });
     //* fetch circles
     fetch(`${serverIp}/fetchcircle`).
-    then(async function(response) {
-        var responseJSON = await response.json()
-        var array = responseJSON.circle
-        if (array.length != 0) {
-            array.forEach(async element => {
-                var geojson = await JSON.parse(element.geojson);
-                L.circle(geojson.geometry.coordinates, element.radius, {
-                    shapeOptions: {
-                        stroke: true,
-                        color: '#3388ff',
-                        weight: 4,
-                        opacity: 0.5,
-                        fill: true,
-                        fillColor: null,
-                        fillOpacity: 0.2,
-                        clickable: true
-                    },
-                    showRadius: true,
-                    metric: true,
-                    feet: true,
-                    nautic: false
-                }).addTo(drawnItems);
-            });
-        }
-    });
+        then(async function (response) {
+            var responseJSON = await response.json();
+            var array = responseJSON.circle;
+            if (array.length != 0) {
+                array.forEach(async element => {
+                    var coordinates = await JSON.parse(element.latlng);
+                    L.circle(rc.unproject(coordinates), element.radius, {
+                        shapeOptions: {
+                            stroke: true,
+                            color: '#3388ff',
+                            weight: 4,
+                            opacity: 0.5,
+                            fill: true,
+                            fillColor: null,
+                            fillOpacity: 0.2,
+                            clickable: true
+                        },
+                        showRadius: true,
+                        metric: true,
+                        feet: true,
+                        nautic: false
+                    }).addTo(drawnItems);
+                });
+            }
+        });
     //* fetch markers
     fetch(`${serverIp}/fetchmarker`)
-    .then(async function(response) {
-        var responseJSON = await response.json()
-        var array = responseJSON.marker
-        if (array.length != 0) {
-            array.forEach(element => {
-                elementArray = JSON.parse(element)
-                L.marker(elementArray.geometry.coordinates, {
-                    icon: new L.Icon.Default(),
-                    repeatMode: false,
-                    zIndexOffset: 2000
-                }).addTo(drawnItems);
-            });
-        }
-    });
+        .then(async function (response) {
+            var responseJSON = await response.json();
+            var array = responseJSON.marker;
+            if (array.length != 0) {
+                array.forEach(async element => {
+                    coordinates = await JSON.parse(element);
+                    L.marker(rc.unproject(coordinates), {
+                        icon: new L.Icon.Default(),
+                        repeatMode: false,
+                        zIndexOffset: 2000
+                    }).addTo(drawnItems);
+                });
+            }
+        });
 }
 
 //! on creation of drawing
@@ -138,39 +142,42 @@ map.on(L.Draw.Event.CREATED, function (e) {
         layer = e.layer;
 
     if (type === "polyline") {
-        var GeoJSONLayer = layer.toGeoJSON();
+        var latlng = [];
+        layer.getLatLngs().forEach(array => {
+            latlng.push(rc.project(array));
+        });
         fetch(`${serverIp}/polyline`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                polyline: JSON.stringify(GeoJSONLayer)
+                latlng: JSON.stringify(latlng)
             })
         });
     } else
         if (type === "circle") {
-            var GeoJSONLayer = layer.toGeoJSON();
+            var coordinates = rc.project(layer.getLatLng());
             fetch(`${serverIp}/circle`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    circle: JSON.stringify(GeoJSONLayer),
+                    latlng: JSON.stringify(coordinates),
                     radius: layer.getRadius()
                 })
             });
         } else
             if (type === "marker") {
-                var GeoJSONLayer = layer.toGeoJSON();
+                var coordinates = rc.project(layer.getLatLng());
                 fetch(`${serverIp}/marker`, {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
                     },
                     body: JSON.stringify({
-                        marker: JSON.stringify(GeoJSONLayer)
+                        latlng: JSON.stringify(coordinates)
                     })
                 });
             }
@@ -183,39 +190,44 @@ map.on(L.Draw.Event.DELETED, function (e) {
     var layers = e.layers;
     layers.eachLayer(layer => {
         if (layer instanceof L.Polyline && !(layer instanceof L.Polygon)) {
-            var GeoJSONLayer = layer.toGeoJSON();
+            var latlng = [];
+            layer.getLatLngs().forEach(array => {
+                latlng.push(rc.project(array));
+            });
+            console.log("fetch")
+            console.log(JSON.stringify(latlng))
             fetch(`${serverIp}/deletepolyline`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    polyline: JSON.stringify(GeoJSONLayer)
+                    latlng: JSON.stringify(latlng)
                 })
             });
         } else
             if (layer instanceof L.Circle) {
-                var GeoJSONLayer = layer.toGeoJSON();
+                var coordinates = rc.project(layer.getLatLng());
                 fetch(`${serverIp}/deletecircle`, {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
                     },
                     body: JSON.stringify({
-                        circle: JSON.stringify(GeoJSONLayer),
+                        latlng: JSON.stringify(coordinates),
                         radius: layer.getRadius()
                     })
                 });
             } else
                 if (layer instanceof L.Marker) {
-                    var GeoJSONLayer = layer.toGeoJSON();
+                    var coordinates = rc.project(layer.getLatLng());
                     fetch(`${serverIp}/deletemarker`, {
                         method: 'POST',
                         headers: {
                             'Content-Type': 'application/json'
                         },
                         body: JSON.stringify({
-                            marker: JSON.stringify(GeoJSONLayer)
+                            latlng: JSON.stringify(coordinates)
                         })
                     });
                 }
--- Server/server.js
+++ Server/server.js
@@ -20,17 +20,17 @@ let db = new sqlite3.Database("./db/database.db", sqlite3.OPEN_READWRITE | sqlit
     }
     console.log("connected to Reminder db");
     //* creates table for reminders
-    db.run(/*sql*/`CREATE TABLE IF NOT EXISTS "PolyLine" ("GeoJSON" TEXT NOT NULL)`, function (err) {
+    db.run(/*sql*/`CREATE TABLE IF NOT EXISTS "PolyLine" ("LatLng" TEXT NOT NULL)`, function (err) {
         if (err) {
             console.error(err.message);
         }
     });
-    db.run(/*sql*/`CREATE TABLE IF NOT EXISTS "Circle" ("GeoJSON" TEXT NOT NULL, "Radius" INTEGER NOT NULL)`, function (err) {
+    db.run(/*sql*/`CREATE TABLE IF NOT EXISTS "Circle" ("LatLng" TEXT NOT NULL, "Radius" INTEGER NOT NULL)`, function (err) {
         if (err) {
             console.error(err.message);
         }
     });
-    db.run(/*sql*/`CREATE TABLE IF NOT EXISTS "Marker" ("GeoJSON" TEXT NOT NULL)`, function (err) {
+    db.run(/*sql*/`CREATE TABLE IF NOT EXISTS "Marker" ("LatLng" TEXT NOT NULL)`, function (err) {
         if (err) {
             console.error(err.message);
         }
@@ -43,7 +43,7 @@ app.post('/polyline', function (req, res) {
     // get sent data
     var input = req.body;
     // insert
-    db.run(/*sql*/`INSERT INTO PolyLine VALUES (?)`, [input['polyline']]);
+    db.run(/*sql*/`INSERT INTO PolyLine VALUES (?)`, [input['latlng']]);
     res.end('Success');
 });
 app.post('/circle', function (req, res) {
@@ -51,7 +51,7 @@ app.post('/circle', function (req, res) {
     // get sent data
     var input = req.body;
     // insert
-    db.run(/*sql*/`INSERT INTO Circle VALUES (?, ?)`, [input['circle'], input['radius']]);
+    db.run(/*sql*/`INSERT INTO Circle VALUES (?, ?)`, [input['latlng'], input['radius']]);
     res.end('Success');
 });
 app.post('/marker', function (req, res) {
@@ -59,35 +59,109 @@ app.post('/marker', function (req, res) {
     // get sent data
     var input = req.body;
     // insert
-    db.run(/*sql*/`INSERT INTO Marker VALUES (?)`, [input['marker']]);
+    db.run(/*sql*/`INSERT INTO Marker VALUES (?)`, [input['latlng']]);
     res.end('Success');
 });
 
 
 //! Receive deletion notices
-app.post('/deletepolyline', function (req, res) {
+app.post('/deletepolyline', async function (req, res) {
     console.log("fetched");
     // get sent data
     var input = req.body;
-    // delete
-    db.run(/*sql*/`DELETE FROM PolyLine WHERE GeoJSON = (?)`, [input['polyline']]);
-    res.end('Success');
+    let sql = /*sql*/`SELECT    LatLng,
+                                _rowid_ id
+                        FROM PolyLine
+                        ORDER BY _rowid_`;
+    db.all(sql, [], async (err, rows) => {
+        if(err) {
+            res.end('Failure')
+            console.log("fail")
+        }
+        else {
+            var inputObj = await JSON.parse(input['latlng'])
+            rows.forEach(async function(row) {
+                var same = true;
+                var rowArray = await JSON.parse(row.LatLng)
+                rowArray.forEach(function(arrayRow, index) {
+                    var arrayInput = inputObj[index]
+                    var inputX = (Math.round(arrayInput.x * 10000000)) / 10000000;
+                    var inputY = (Math.round(arrayInput.y * 10000000)) / 10000000;
+                    var rowX = (Math.round(arrayRow.x * 10000000)) / 10000000;
+                    var rowY = (Math.round(arrayRow.y * 10000000)) / 10000000;
+                    if (!(inputX === rowX && inputY === rowY)) {
+                        same = false;
+                    }
+                })
+                if (same) {
+                    db.run(/*sql*/`DELETE FROM PolyLine WHERE _rowid_ = (?)`, [row.id])
+                    res.end('Success')
+                }
+            })
+        }
+    })
 });
-app.post('/deletecircle', function (req, res) {
+
+app.post('/deletecircle', async function (req, res) {
     console.log("fetched");
     // get sent data
     var input = req.body;
-    // delete
-    db.run(/*sql*/`DELETE FROM Circle WHERE GeoJSON = (?) AND Radius = (?)`, [input['circle'], input['radius']]);
-    res.end('Success');
+    let sql = /*sql*/`SELECT    LatLng,
+                                Radius,
+                                _rowid_ id
+                        FROM Circle
+                        ORDER BY _rowid_`;
+    db.all(sql, [], async (err, rows) => {
+        if(err) {
+            res.end('Failure')
+            console.log("fail")
+        }
+        else {
+            rows.forEach(async function(row) {
+                var arrayInput = await JSON.parse(input['latlng'])
+                var arrayRow = await JSON.parse(row.LatLng)
+                var inputRadius = await JSON.parse(input['radius'])
+                var rowRadius = row.radius
+                var inputX = (Math.round(arrayInput.x * 10000000)) / 10000000;
+                var inputY = (Math.round(arrayInput.y * 10000000)) / 10000000;
+                var rowX = (Math.round(arrayRow.x * 10000000)) / 10000000;
+                var rowY = (Math.round(arrayRow.y * 10000000)) / 10000000;
+                if (inputX === rowX && inputY === rowY && inputRadius == rowRadius) {
+                    db.run(/*sql*/`DELETE FROM Circle WHERE _rowid_ = (?)`, [row.id])
+                    res.end('Success')
+                }
+            })
+        }
+    })
 });
 app.post('/deletemarker', function (req, res) {
     console.log("fetched");
     // get sent data
     var input = req.body;
-    // delete
-    db.run(/*sql*/`DELETE FROM Marker WHERE GeoJSON = (?)`, [input['marker']]);
-    res.end('Success');
+    let sql = /*sql*/`SELECT    LatLng,
+                                _rowid_ id
+                        FROM Marker
+                        ORDER BY _rowid_`;
+    db.all(sql, [], async (err, rows) => {
+        if(err) {
+            res.end('Failure')
+            console.log("fail")
+        }
+        else {
+            rows.forEach(async function(row) {
+                var arrayInput = await JSON.parse(input['latlng'])
+                var arrayRow = await JSON.parse(row.LatLng)
+                var inputX = (Math.round(arrayInput.x * 10000000)) / 10000000;
+                var inputY = (Math.round(arrayInput.y * 10000000)) / 10000000;
+                var rowX = (Math.round(arrayRow.x * 10000000)) / 10000000;
+                var rowY = (Math.round(arrayRow.y * 10000000)) / 10000000;
+                if (inputX === rowX && inputY === rowY) {
+                    db.run(/*sql*/`DELETE FROM Marker WHERE _rowid_ = (?)`, [row.id])
+                    res.end('Success')
+                }
+            })
+        }
+    })
 });
 
 
@@ -95,7 +169,7 @@ app.post('/deletemarker', function (req, res) {
 app.get('/fetchpolyline', function (_, res) {
     console.log("fetched");
     var output = { polyline: [] };
-    let sql = /*sql*/`SELECT    GeoJSON
+    let sql = /*sql*/`SELECT    LatLng
                         FROM PolyLine
                         ORDER BY _rowid_`;
     db.all(sql, [], (err, rows) => {
@@ -106,7 +180,7 @@ app.get('/fetchpolyline', function (_, res) {
         else {
             if (rows.length != 0) {
                 rows.forEach(function (row) {
-                    output.polyline.push(row.GeoJSON);
+                    output.polyline.push(row.LatLng);
                 });
                 res.send(JSON.stringify(output));
                 res.end("Success");
@@ -122,7 +196,7 @@ app.get('/fetchpolyline', function (_, res) {
 app.get('/fetchcircle', function (_, res) {
     console.log("fetched");
     var output = { circle: [] };
-    let sql = /*sql*/`SELECT    GeoJSON,
+    let sql = /*sql*/`SELECT    LatLng,
                                 Radius
                         FROM Circle
                         ORDER BY _rowid_`;
@@ -134,7 +208,7 @@ app.get('/fetchcircle', function (_, res) {
         else {
             if (rows.length != 0) {
                 rows.forEach(function (row) {
-                    output.circle.push({ geojson: row.GeoJSON, radius: row.Radius });
+                    output.circle.push({ latlng: row.LatLng, radius: row.Radius });
                 });
                 res.send(JSON.stringify(output));
                 res.end("Success");
@@ -150,7 +224,7 @@ app.get('/fetchcircle', function (_, res) {
 app.get('/fetchmarker', function (_, res) {
     console.log("fetched");
     var output = { marker: [] };
-    let sql = /*sql*/`SELECT    GeoJSON
+    let sql = /*sql*/`SELECT    LatLng
                         FROM Marker
                         ORDER BY _rowid_`;
     db.all(sql, [], (err, rows) => {
@@ -161,7 +235,7 @@ app.get('/fetchmarker', function (_, res) {
         else {
             if (rows.length != 0) {
                 rows.forEach(function (row) {
-                    output.marker.push(row.GeoJSON);
+                    output.marker.push(row.LatLng);
                 });
                 console.log("send");
                 res.send(JSON.stringify(output));
